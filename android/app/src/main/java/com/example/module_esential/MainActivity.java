fc73d88881c6ecb02c29079709851ad783192f0b
Salmanakash007 <113782902+Salmanakash007@users.noreply.github.com>
2023-09-30 18:53:42 +0600

    Initial commit

---
+++ android/app/src/main/java/com/example/module_esential/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.module_esential;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/home_page.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'dashboard.dart';
+
+import 'dashboard.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.blue,
+            title: Text("Flutter Home Page"),
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.pink,
+                        textStyle: TextStyle(fontWeight: FontWeight.bold)),
+                    onLongPress: () {},
+                    onPressed: () {
+                      print("Active");
+                    },
+                    child: Text(
+                      "Need Gold",
+                      style: TextStyle(color: Colors.white),
+                    )),
+                TextButton(
+                    onPressed: () {
+                      print("onpress");
+                    },
+                    onLongPress: () {
+                      print("long press");
+                    },
+                    child: Text("Click it")),
+                IconButton(onPressed: () {}, icon: Icon(Icons.add)),
+                TextField(
+                  controller: TextEditingController(),
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      // borderRadius: BorderRadius.all(Radius.zero)
+                        borderRadius: BorderRadius.circular(50)
+                    ),
+                      label: Text("Email Adress"),
+                      labelStyle: TextStyle(color: Colors.pink),
+                      hintText: "Enter emsil adress",
+                      hintStyle: TextStyle(
+                          color: Colors.pink, fontWeight: FontWeight.bold),
+                     fillColor: Colors.grey ,
+                    filled: true,
+                    suffixIcon: Icon(Icons.email,color: Colors.pink,),
+
+                  ),
+                ),
+                SizedBox(height: 10),
+                TextField(
+                  controller: TextEditingController(),
+                  obscureText:true,
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      // borderRadius: BorderRadius.all(Radius.zero)
+                      borderRadius: BorderRadius.circular(50)
+                    ),
+                    label: Text("Email Adress"),
+                    labelStyle: TextStyle(color: Colors.pink),
+                    hintText: "Enter emsil adress",
+                    hintStyle: TextStyle(
+                        color: Colors.pink, fontWeight: FontWeight.bold),
+                    fillColor: Colors.grey ,
+                    filled: true,
+                    suffixIcon: Icon(Icons.password,color: Colors.pink,),
+
+                  ),
+                ),
+                E
+              ],
+            ),
+          )),
+    );
+  }
+}
